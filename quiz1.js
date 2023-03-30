6357a3ffdd658e32686c98e433be7a9d9db97a98
SAM ZHI KANG <b022120028@student.utem.edu.my>
2023-03-30 15:54:30 +0800

    code 30.3.23

---
+++ quiz1.js
@@ -0,0 +1,57 @@
+//var massege="benr"
+//console.log(massege)
+//console.log(typeof massege)
+
+//let str=sam
+//let num=3523
+//let course=str +num
+//console.log(course, typeof course)
+
+//let str="1"
+//let num=1
+//let course=str +num
+//console.log(course, typeof course)
+
+//let str="1"
+//let num=1
+//let course=parseInt(str) +num
+//console.log(course, typeof course)
+
+//let str="1.333"
+//let num=1
+//let course=parseFloat(str) +num
+//console.log(course, typeof course)
+
+//array1
+//var arr=["sam",1,"e"]
+//console.log(arr[2])
+
+//array1
+//var arr=[["sam",1,"e"],["szk",3,"t"]]
+//console.log(arr[1][1])
+
+//object
+//var arr={
+//    name:"sam",
+//    age:55,
+//    gentle:"male"}
+//console.log(arr.name)
+//console.log(arr["name"])
+
+//var user={age:{age1:22,age2:24,age3:32},
+//         name:{name1:"Soo",name2:"Sam",name3:"KK"}}
+//console.log(user.age)
+
+let message=[
+    {name:"usr1",
+    date:"1.1.23",
+    msg:"good",
+    like:1000},
+    {name:"usr2",
+    date:"3.3.23",
+    msg:"gooderrr",
+    like:10000
+
+    }
+]
+console.log(message[1].msg)
